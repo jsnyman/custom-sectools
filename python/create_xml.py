4fe6f27d740c73f42c409750d967b79474648dc7
jsnyman <johan_snyman@yahoo.com>
2016-02-17 12:20:45 +0200

    Update create_xml.py

--- python/create_xml.py
+++ python/create_xml.py
@@ -2,6 +2,20 @@
 
 import xml.etree.ElementTree as ET
 
+""" Creates the following XML Tree (change as is needed):
+<?xml version="1.0" ?>
+<soapenv:Envelope xmlns:soap="http://schemas.microsoft.com/sharepoint/soap/" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+  <soapenv:Header/>
+  <soapenv:Body>
+    <soap:SearchPrincipals>
+      <soap:searchText>a</soap:searchText>
+      <soap:maxResults>100</soap:maxResults>
+      <soap:principalType>All</soap:principalType>
+    </soap:SearchPrincipals>
+  </soapenv:Body>
+</soapenv:Envelope>
+"""
+
 def createXml():
   root = ET.Element("soapenv:Envelope", {
     "xmlns:soapenv" : "http://schemas.xmlsoap.org/soap/envelope/",
